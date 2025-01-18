9a09aa5fa44aa0526526d0a147bd0418e7032137
ifilot <ivo@ivofilot.nl>
2025-01-18 22:43:55 +0100

    Finalizing terminal echo program

--- programs/acia/acia.asm
+++ programs/acia/acia.asm
@@ -12,6 +12,8 @@
 .define ACIA_CMD        $9F06
 .define ACIA_CTRL       $9F07
 
+.define ESC		$1B		; VT100 escape character
+
 .segment "CODE"
 
 ;-------------------------------------------------------------------------------
@@ -41,11 +43,18 @@ init:
     sta ACIA_CTRL 	; Write to ACIA control register
     lda #%00001001      ; No parity, no echo, no interrupts.
     sta ACIA_CMD
-    lda #'@'            ; print '@' on boot
-    jsr charout
-    lda #':'
-    jsr charout
+    lda #<termbootstr   ; load lower byte
+    sta $10
+    lda #>termbootstr   ; load upper byte
+    sta $11
+    jsr stringout
+    jsr nlout
     rts
+
+termbootstr:
+    .byte ESC,"[2J",ESC,"[HByteCradle 6502",ESC,"[B",ESC,"[1G","ACIA echo test",0
+newlinestr:
+    .byte ESC,"[B",ESC,"[1G@:",0
         
 ;-------------------------------------------------------------------------------
 ; MAIN routine
@@ -55,7 +64,41 @@ main:
 
 
 ;-------------------------------------------------------------------------------
+; NLOUT routine
+; Garbles: X,y
+;
+; Moves the cursor to the next line
+;-------------------------------------------------------------------------------
+nlout:
+    lda #<newlinestr	; load lower byte
+    sta $10
+    lda #>newlinestr	; load upper byte
+    sta $11
+    jsr stringout
+    rts
+
+;-------------------------------------------------------------------------------
+; STRINGOUT routine
+; Garbles: A, Y
+;
+; Loops over a string and print its characters until a zero-terminating character 
+; is found. Assumes that $10 is used on the zero page to store the address of
+; the string.
+;-------------------------------------------------------------------------------
+stringout:
+    ldy #0
+@nextchar:
+    lda ($10),y		; load character from string
+    beq @exit		; if terminating character is read, exit
+    jsr charout		; else, print char
+    iny			; increment y
+    jmp @nextchar	; read next char
+@exit:
+    rts
+
+;-------------------------------------------------------------------------------
 ; CHAROUT routine
+; Garbles: A
 ;
 ; Prints a character to the ACIA; note that software delay is needed to prevent
 ; transmitting data to the ACIA while it is still transmitting. At 10 MhZ, we
@@ -63,6 +106,7 @@ main:
 ; 256 * 5 = 1280 cycles. Thus, we take 5 outer loops and have some margin.
 ;-------------------------------------------------------------------------------
 charout:
+    phx			; preserve X
     sta ACIA_DATA    	; write the character to the ACIA data register
     ldx #5              ; number of outer loops, see description above 
 delay_outer:
@@ -72,6 +116,7 @@ delay_inner:
     bne delay_inner     ; check if zero; 3 cycles
     dex                 ; decrement X
     bne delay_outer
+    plx
     rts
 
 ;-------------------------------------------------------------------------------
@@ -89,10 +134,30 @@ isr:
     and #$08		; check for bit 3
     beq isr_exit	; if not set, exit isr
     lda ACIA_DATA	; else transmit the byte back
-    jsr charout 
+    cmp #$0D		; check for CR
+    beq newline         ; print newline
+    cmp #$7F            ; check for delete
+    beq backspace	; run backspace routine
+    jsr charout 	; else do normal print of character
 isr_exit:
     pla			; recover A
     rti
+
+newline:
+    jsr nlout
+    jmp isr_exit
+   
+backspace:
+    lda #$08
+    jsr charout
+    lda #' '
+    jsr charout
+    lda #$08
+    jsr charout
+    jmp isr_exit	; exit interrupt
+
+bsstr:
+    .byte ESC,"[D ",ESC,"[D",0
 
 ;-------------------------------------------------------------------------------
 ; Vectors

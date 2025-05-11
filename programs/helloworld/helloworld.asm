e9a430fd6102ab15f6d2b7ff7a79253b9a485e35
ifilot <ivo@ivofilot.nl>
2025-05-11 09:09:30 +0200

    Adding tutorials and explanation on program deployment

--- programs/helloworld/helloworld.asm
+++ programs/helloworld/helloworld.asm
@@ -1,29 +1,32 @@
-; this code is a simple monitor class for the ByteCradle 6502
+;------------------------------------------------------------------------------
+; Hello World Example for ByteCradle 6502
+;
+; This program prints "Hello World!" to the screen using the ByteCradle OS's
+; built-in output routine located at $FFF4.
+;------------------------------------------------------------------------------
 
-.PSC02
+.PSC02                              ; Assembly for the 65C02 CPU
 
-.import __HEADER_LOAD__             ; will be grabbed from the .cfg file
+.import __HEADER_LOAD__             ; Provided by the linker (.cfg) as load address
 
-.define PUTSTRNL            $FFF4   ; print string routine
+.define PUTSTRNL $FFE8              ; Routine to print a null-terminated string with newline
 
-;-------------------------------------------------------------------------------
-; PROGRAM HEADER
-;-------------------------------------------------------------------------------
-
+;------------------------------------------------------------------------------
+; Program Header (used by ByteCradle OS to determine deployment address)
+;------------------------------------------------------------------------------
 .segment "HEADER"
+.word __HEADER_LOAD__               ; Deployment address (typically $0800), little-endian
 
-.word __HEADER_LOAD__               ; program location
-
+;------------------------------------------------------------------------------
+; Main Code Segment
+;------------------------------------------------------------------------------
 .segment "CODE"
 
-;-------------------------------------------------------------------------------
-; Start sequence
-;-------------------------------------------------------------------------------
-start:                              ; reset vector points here
-    lda #<hwstr		                ; load lower byte
-    ldx #>hwstr		                ; load upper byte
-    jsr PUTSTRNL
-    rts
+start:                              ; Entry point
+    lda #<hwstr                     ; Load low byte of string address
+    ldx #>hwstr                     ; Load high byte of string address
+    jsr PUTSTRNL                    ; Call OS routine to print string with newline
+    rts                             ; Return to OS
 
 hwstr:
-    .asciiz "Hello World!"
+    .asciiz "Hello World!"          ; Null-terminated string
--- src/bcos/jumptable.s
+++ src/bcos/jumptable.s
@@ -1,26 +1,24 @@
-.import getch
-.import newline
-.import puthex
-.import putdec
-.import putch
-.import putstrnl
-.import putstr
+;
+; ByteCradle OS Jump Table
+;
+; This jump table exposes a set of core subroutines that user programs can call
+; at fixed addresses in ROM. Each entry is a `jmp` to the corresponding routine.
+; By calling these jump points instead of the functions directly, compatibility
+; can be preserved even if internal function addresses change.
+;
+; These entry points are located at the top of the address space (starting at $FFE5),
+; similar to traditional system ROMs.
+;
 
-;-------------------------------------------------------------------------------
-; JUMP TABLE
-;-------------------------------------------------------------------------------
+.include "functions.inc"
+
 .segment "JUMPTABLE"
-    .byte $4C           ; $FFE5
-    .word getch
-    .byte $4C           ; $FFE8
-    .word newline
-    .byte $4C           ; $FFEB
-    .word puthex
-    .byte $4C           ; $FFEE
-    .word putdec
-    .byte $4C           ; $FFF1
-    .word putch
-    .byte $4C           ; FFFF4
-    .word putstrnl
-    .byte $4C           ; $FFF7
-    .word putstr 
+
+jumptable:
+    jmp putstr      ; $FFE5 - Print null-terminated string (X:A = addr)
+    jmp putstrnl    ; $FFE8 - Print string with newline (X:A = addr)
+    jmp putch       ; $FFEB - Print a single character in A
+    jmp newline     ; $FFEE - Print carriage return + line feed
+    jmp puthex      ; $FFF1 - Print byte in A as two-digit hexadecimal
+    jmp putdec      ; $FFF4 - Print byte in A as decimal
+    jmp getch       ; $FFF7 - Get character from key buffer (A)
